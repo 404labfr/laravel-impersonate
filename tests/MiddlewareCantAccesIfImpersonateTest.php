1d747c76228d3e92abf000fd8ac4095aca5c6204
tghpow <contact@thibault-chazottes.fr>
2017-02-17 16:10:06 +0100

    Rename Middleware test
    Begin by Middleware, it’s better if there’ll be other middleware test

--- tests/MiddlewareCantAccesIfImpersonateTest.php
+++ tests/MiddlewareCantAccesIfImpersonateTest.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Lab404\Tests\Stubs\Models\User;
 use Lab404\Impersonate\Middleware\CantAccesIfImpersonate;
 
-class CantAccesIfImpersonateMiddlewareTest extends TestCase
+class MiddlewareCantAccesIfImpersonateTest extends TestCase
 {
     /** @var  User */
     protected $user;
